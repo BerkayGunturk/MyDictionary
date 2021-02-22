2cfcd83beb5e6316a009d18a37c188bc0aa88899
Hasan Aykaç <hasan_tugra1999@outlook.com>
2021-02-22 16:47:45 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] age;
+        T2[] name;
+        public MyDictionary()
+        {
+            age = new T1[0];
+            name = new T2[0];
+        }
+        public void Add(T1 ages, T2 names)
+        {
+            T1[] tempAge = age;
+            T2[] tempName = name;
+
+            age = new T1[age.Length + 1];
+            name = new T2[name.Length + 1];
+
+            for (int i = 0; i < tempAge.Length; i++)
+            {
+                age[i] = tempAge[i];
+            }
+            age[age.Length - 1] = ages;
+
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+            }
+            name[name.Length - 1] = names;
+
+
+
+        }
+        public void ToList()
+        {
+            for (int i = 0; i < age.Length; i++)
+            {
+                Console.WriteLine("Yaş: " + age[i] + " " + "İsim: " + name[i]);
+
+
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customer = new MyDictionary<int, string>();
+
+            customer.Add(22, "Hasan");
+            customer.Add(50, "Bekir");
+            customer.Add(45, "Hanife");
+            customer.Add(18, "Mehmet");
+
+
+            customer.ToList();
+        }
+    }
+}
